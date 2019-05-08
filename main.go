d01db8dc0afff3eb47203c86a6b84272f9b4dd7d
bjhl <bjhl@bjhldeMBP-72.lan>
2019-05-09 00:56:22 +0800

    路由

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
 	"github.com/kataras/iris/middleware/logger"
 	recover2 "github.com/kataras/iris/middleware/recover"
-	"go-web/dto/response"
 	"go-web/routers"
 )
 
@@ -13,12 +11,6 @@ func main() {
 	app := iris.New()
 	app.Use(recover2.New())
 	app.Use(logger.New())
-
-	app.OnErrorCode(iris.StatusNotFound, func(context context.Context) {
-		context.JSON(response.BaseResponse(0, nil, "404 NOT FOUND"))
-	})
-
-	app.PartyFunc("/", routers.Routers)
-
+	routers.InitRoute(app)
 	app.Run(iris.Addr(":8080"))
 }
--- routers/routers.go
+++ routers/routers.go
@@ -1,12 +1,17 @@
 package routers
 
 import (
-	"github.com/kataras/iris/core/router"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
 	"go-web/controller"
+	"go-web/dto/response"
 )
 
-func Routers(routers router.Party) {
+func InitRoute(app *iris.Application) {
+	app.OnErrorCode(iris.StatusNotFound, func(context context.Context) {
+		context.JSON(response.BaseResponse(0, nil, "404 NOT FOUND"))
+	})
 
-	routers.PartyFunc("/test", controller.TestRoute)
+	app.PartyFunc("/test", controller.TestRoute)
 
 }
